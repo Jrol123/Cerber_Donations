e69f5dd6e4215f2fa5bf25fc97bfee7cf439f66e
Daniil Pleshanov <113170005+StarkUnfassbar@users.noreply.github.com>
2024-03-28 21:04:42 +1000

    Add files via upload

--- js/href_chat_mobile.js
+++ js/href_chat_mobile.js
@@ -3,6 +3,8 @@ let hrefChat = document.querySelector(".href_chat");
 let lastScrollPosition = 0;
 
 if (window.innerWidth < 720) {
+    hrefChat.style.pointerEvents = "painted";
+
     let scrollDistanceToBottom = document.documentElement.scrollHeight - (window.scrollY + window.innerHeight);
     let opacityValue = 1 - (scrollDistanceToBottom / 500);
     opacityValue = Math.min(1, Math.max(0, opacityValue));
@@ -19,7 +21,7 @@ if (window.innerWidth < 720) {
 }
 
 window.addEventListener('scroll', function() {
-    if (window.innerWidth < 720) {
+    if (window.innerWidth <= 720) {
         let scrollDistanceToBottom = document.documentElement.scrollHeight - (window.scrollY + window.innerHeight);
         let opacityValue = 1 - (scrollDistanceToBottom / 500);
         opacityValue = Math.min(1, Math.max(0, opacityValue));
@@ -33,8 +35,31 @@ window.addEventListener('scroll', function() {
         }
     
         lastScrollPosition = window.scrollY;
-    } else if(window.innerWidth < 1100){
-        patternImgs[1].style.opacity = 1;
-        hrefChat.style.opacity = 1;
     }
 });
+
+window.addEventListener('resize', function(event){
+    if (window.innerWidth <= 720) {
+        hrefChat.style.pointerEvents = "painted";
+    
+        let scrollDistanceToBottom = document.documentElement.scrollHeight - (window.scrollY + window.innerHeight);
+        let opacityValue = 1 - (scrollDistanceToBottom / 500);
+        opacityValue = Math.min(1, Math.max(0, opacityValue));
+    
+        if (window.scrollY < lastScrollPosition) {
+            patternImgs[1].style.opacity = 1 - opacityValue;
+            hrefChat.style.opacity = 1 - opacityValue;
+        } else {
+            patternImgs[1].style.opacity = opacityValue;
+            hrefChat.style.opacity = opacityValue;
+        }
+    
+        lastScrollPosition = window.scrollY;
+    } else if(window.innerWidth <= 1100){
+        patternImgs[1].style.opacity = 1;
+        hrefChat.style.opacity = 1;
+    } else if(window.innerWidth > 1100){
+        patternImgs[1].style = "";
+        hrefChat.style = "";
+    }
+})
